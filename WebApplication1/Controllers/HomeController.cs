5fe1ba75916ef58724c6cf32c046818ace370002
Marcos Carvalho <mcarvalho@sestsenat.org.br>
2019-10-08 17:47:58 -0300

    Initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if(HttpContext.Cache["ObjectList"] == null)
+            {
+                HttpContext.Cache["ObjectList"] = new List<Models.Msg>();
+            }
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult getMgss()
+        {
+            List<Models.Msg> msgss = (List<Models.Msg>)HttpContext.Cache["ObjectList"];
+
+            //Models.Msg msg = new Models.Msg
+            //{
+            //    nome = "pedin",
+            //    msg = DateTime.Now.ToString(),
+            //    id = msgss.Count+1
+            //};
+
+            //msgss.Add(msg);
+
+            return Json(msgss.OrderByDescending(s => s.id), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult setMsg(string msg, string nome)
+        {
+            List<Models.Msg> msgss = (List<Models.Msg>)HttpContext.Cache["ObjectList"];
+
+            Models.Msg omsg = new Models.Msg
+            {
+                nome = nome,
+                msg = msg,
+                id = msgss.Count + 1
+            };
+
+            msgss.Add(omsg);
+
+            try
+            {
+                return Json(msgss, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Msg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Msg
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string msg { get; set; }
+    }
+}
